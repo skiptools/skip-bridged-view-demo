5e50616f2d31c6b404562fa7c0f70f752727ddc0
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-04-14 18:07:44 -0400

    Update demo view

--- Sources/SkipBridgedViewDemo/SkipBridgeViewDemo.swift
+++ Sources/SkipBridgedViewDemo/SkipBridgeViewDemo.swift
@@ -1,12 +1,9 @@
 // Copyright 2024–2025 Skip
 // SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
-#if !SKIP_BRIDGE
 import Foundation
-#if canImport(SkipFuseUI)
-import SkipFuseUI
-#else
+#if !SKIP_BRIDGE
+#if !SKIP
 import SwiftUI
-#endif
 
 /// Example of a view that can be used in a native `SkipFuseUI` app.
 public struct SkipBridgedDemoView : View {
@@ -14,27 +11,34 @@ public struct SkipBridgedDemoView : View {
     }
 
     public var body: some View {
-        #if SKIP
-//        ComposeView {
-//            HeartComposer()
-//        }
-        ComposeView { ctx in // Mix in Compose code!
-            androidx.compose.material3.Text("💚", modifier: ctx.modifier)
-        }
-        #else
         Text(verbatim: "💙 Hello Darwin!")
-        #endif
     }
 }
+#endif
+#endif
 
+#if SKIP_BRIDGE
+import SkipFuseUI
 
-//#if SKIP
-///// Use a ContentComposer to integrate Compose content. This code will be transpiled to Kotlin.
-//struct HeartComposer : ContentComposer {
-//    @Composable func Compose(context: ComposeContext) {
-//        androidx.compose.material3.Text("💚 Hello Android!", modifier: context.modifier)
-//    }
-//}
-//#endif
+public struct SkipBridgedDemoView : View {
+    public init() {
+    }
 
+    public var body: some View {
+        ComposeView {
+            DemoComposer()
+        }
+    }
+}
+#endif
+
+#if SKIP
+import SkipUI
+
+/// Use a ContentComposer to integrate Compose content. This code will be transpiled to Kotlin.
+struct DemoComposer : ContentComposer {
+    @Composable func Compose(context: ComposeContext) {
+        androidx.compose.material3.Text("💚 Hello Android!", modifier: context.modifier)
+    }
+}
 #endif
